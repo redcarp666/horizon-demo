53cea399f4481b122e271e54a93da015a8eab0bb
sushuaihao <sushuaihao@163.com>
2024-02-29 17:58:23 +0800

    引入测试

---
+++ horizon-demo-system/src/test/java/org/redcarp/horizon/demo/system/test/Test.java
@@ -0,0 +1,11 @@
+package org.redcarp.horizon.demo.system.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author redcarp
+ * @date 2024/2/29
+ */
+@SpringBootTest
+public class Test {
+}
