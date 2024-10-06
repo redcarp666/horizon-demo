1a46c3aa549a904ef0fedd1e9e48bb8107f1e23d
sushuaihao <sushuaihao@163.com>
2024-10-06 18:10:43 +0800

    init

---
+++ horizon-demo-test/src/test/java/org/redcarp/horizon/test/Test.java
@@ -0,0 +1,23 @@
+package org.redcarp.horizon.test;
+
+import org.apache.poi.ss.formula.functions.T;
+
+/**
+ * @author redcarp
+ * @date 2024/7/24
+ */
+public class Test {
+	public static void main(String[] args) throws InterruptedException {
+		Thread thread = new Thread(() -> {
+			try {
+				Thread.sleep(200000);
+				System.out.println("hello,i'm thread");
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		});
+		thread.start();
+		thread.join(10000);
+		System.out.println("hello world");
+	}
+}
